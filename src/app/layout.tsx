d2384760f5285987b9a62d1bf171cf906b47e41e
rrh1441 <rrh1441@gmail.com>
2025-02-04 16:23:55 -0800

    Updated stripe for production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <Analytics /> {/* This ensures Analytics is used and tracked */}
+        <Analytics /> {/* Thi s ensures Analytics is used and tracked */}
       </body>
     </html>
   )
