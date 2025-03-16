089c172f0a6384e6928da7db25d399975875e97e
rrh1441 <rrh1441@gmail.com>
2025-03-16 16:34:28 -0700

    Add missing separator component to resolve build error v3

--- src/app/components/ui/separator.tsx
+++ src/app/components/ui/separator.tsx
@@ -3,13 +3,16 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-export interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {}
+type SeparatorProps = React.HTMLAttributes<HTMLDivElement>;
 
 const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
   ({ className, ...props }, ref) => {
-    return <div ref={ref} className={cn("h-px bg-gray-200 my-4", className)} {...props} />;
+    return (
+      <div ref={ref} className={cn("h-px bg-gray-200 my-4", className)} {...props} />
+    );
   }
 );
+
 Separator.displayName = "Separator";
 
 export { Separator };
